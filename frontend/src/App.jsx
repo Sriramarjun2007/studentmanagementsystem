f22b1532cc127b4b7404b8deb2192d36fadb2b27
Sriram Arjun <sriramarjun2007@gmail.com>
2026-07-25 11:42:05 +0530

    Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/AddStudent";
 import AddStudent from "./pages/AddStudent";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Addstudent" element={<AddStudent/>} />
+      <Route path="/" element={<AddStudent />} />
     </Routes>
   );
 }
